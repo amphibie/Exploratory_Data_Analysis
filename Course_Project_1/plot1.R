3754b6389778791680b51ca667b3b11465e3d36c
Paul Foellbach <Foellbach@gmx.net>
2015-02-05 15:06:34 +0100

    Course_Project_1

--- Course_Project_1/plot1.R
+++ Course_Project_1/plot1.R
@@ -1,5 +1,16 @@
+# Downloading File
+dataset_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(dataset_url, "exdata_data_household_power_consumption.zip")
+unzip("exdata_data_household_power_consumption.zip", exdir = "exdata_data_household_power_consumption")
+getwd()
+list.files("exdata_data_household_power_consumption")
+#list.files mit dem Argument full.names=TRUE gibt den Filenamen und den Pfad dorthin vom arbeitsverzeichnis aus
+file <- list.files("exdata_data_household_power_consumption", full.names=TRUE)
+
+# Reading File in R
 data<- read.table(file, sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings= "?")
 dim(data)
+
 # Subsetting the full data to obtain the data related to two days: 
 data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
 fix(data)
